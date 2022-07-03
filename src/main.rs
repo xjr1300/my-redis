c9383f0199a4502a16d9cf653d4b8dfaabebfd30
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-03 11:26:02 +0900

    mini-redisにキーと値を設定した後、そのキーの値を取得するコードを実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    // mini-redisアドレスへのコネクションを開く
+    let mut client = client::connect("localhost:6379").await?;
+
+    // "hello"というキーに"world"という値をセット
+    client.set("hello", "world".into()).await?;
+
+    // キー"hello"の値を取得
+    let result = client.get("hello").await?;
+
+    println!("サーバから値を取得しました; result={:?}", result);
+
+    Ok(())
 }
