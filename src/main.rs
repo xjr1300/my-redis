0ac2ec36dc01ca162b82bd85113720436568da22
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-03 16:34:38 +0900

    process関数が1つのソケットで、複数のコマンドを処理できるように実装

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,42 @@ pub async fn main() -> Result<()> {
     }
 }
 
+/// 入ってくるコマンドを処理するために`process`関数を実装する。
+/// 値を蓄積するために`HashMap`を使用する。
+/// `SET`コマンドは`HashMap`の中に値を挿入して、`GET`はそれらを読み出す。
+/// 加えて、接続につき1つのコマンドより多く受け付けるためにループを使用する。
 async fn process(socket: TcpStream) {
-    // `Connection`はバイトストリームの代わりに、Redisフレームを読み書きさせてくれる。
-    // `Connection`型はmini-redisによって定義されている。
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    // データを蓄積するために`HashMap`を使用する。
+    let mut db = HashMap::new();
+
+    // `mini-redis`が提供する`Connection`はソケットから来るフレームを解析処理する。
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("獲得: {:?}", frame);
+    // 接続から来るコマンドを受け取るために`read_frame`を使用する。
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                // `Vec<u8>`として値を蓄積する。
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk`はデータがBytes`型であることを想定する。
+                    // この型はチュートリアルの後半で説明する。
+                    // 現在のところ、`&Vec<u8>`は`into()`の使用することで`Bytes`に変換される。
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("実装されていません。{:?}", cmd),
+        };
 
-        // エラーで応答する。
-        let response = Frame::Error("実装していません。".to_string());
+        // クライアントへの応答を記述する。
         connection.write_frame(&response).await.unwrap();
     }
 }
